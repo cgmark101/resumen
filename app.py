3b031a20b22d5468588f0cc945707886d33b1157
Marcos <cgmark101@gmail.com>
2021-03-09 12:14:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from datetime import datetime, date
 from deta import Deta 
@@ -21,7 +20,6 @@ db = deta.Base("contact")
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
